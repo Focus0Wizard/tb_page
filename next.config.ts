24546eb4ef0a5a0b33d138a3f801815773d7c6f2
Focus0Wizard <jhonn.ramirez@ucb.edu.bo>
2026-08-21 01:24:56 -0400

    command fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', 
+  basePath: '/TB_PAGE'
 };
 
 export default nextConfig;
